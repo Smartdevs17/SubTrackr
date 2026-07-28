f2e8868efe7dd5e6a6f596a72b4e8c714618008e
susanyusuf <susanyusuf1@gmail.com>
2026-07-28 16:44:46 +0100

    fix(ci): prettier-format lint cleanup

--- src/screens/PauseResumeScreen.tsx
+++ src/screens/PauseResumeScreen.tsx
@@ -33,13 +33,8 @@ const PauseResumeScreen: React.FC<Props> = ({ route }) => {
   const colors = useThemeColors();
   const styles = useMemo(() => createStyles(colors), [colors]);
 
-  const {
-    subscriptions,
-    isLoading,
-    pauseSubscription,
-    resumeSubscription,
-    getPauseHistory,
-  } = useSubscriptionStore();
+  const { subscriptions, isLoading, pauseSubscription, resumeSubscription, getPauseHistory } =
+    useSubscriptionStore();
 
   const subscription = subscriptions.find((s) => s.id === subscriptionId);
   const pauseHistory = useMemo(
