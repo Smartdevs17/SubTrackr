614cba2dbb3a7089e622b96ea7c630a016933ef4
akansmafengadamu <akansmafengadamu@gmail.com>
2026-09-23 15:26:15 +0100

    refactor(store): migrate credit/metering/payment/batch/analytics stores to slices (Closes #944)

--- src/store/meteringStore.ts
+++ src/store/meteringStore.ts
@@ -1,2 +1,19 @@
-export { useMeteringStore } from '../../app/stores/meteringStore';
-export type { MeteringState } from '../../app/stores/meteringStore';
+export {
+  useMeteringStore,
+  DEFAULT_PERIOD_SECS,
+  bucketStart,
+  billableUnits,
+} from '../../app/stores/meteringStore';
+export type {
+  MeteringSlice,
+  MeteringStoreState,
+  MeteredUsage,
+  UsageBucket,
+  MeterState,
+  ChargeLine,
+  Charge,
+  TimeRange,
+  UsageAlertEntry,
+  UsageTrend,
+  UsageAnalytics,
+} from '../../app/stores/meteringStore';
