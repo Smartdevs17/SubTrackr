9d7139f5660df21feac1869e07fd2adf2af6d29e
nissinanlung <nissinanlung@gmail.com>
2026-09-24 23:17:19 +0100

    chore: export FunnelQueryHandler from query index (closes #1148)

--- backend/analytics/query/index.ts
+++ backend/analytics/query/index.ts
@@ -4,3 +4,6 @@ export { CohortRetentionQueryHandler } from './cohortRetentionQueryHandler';
 export type { CohortRetentionResult, CohortRetentionPeriod } from './cohortRetentionQueryHandler';
 export { LTVQueryHandler } from './ltvQueryHandler';
 export type { LTVQueryResult } from './ltvQueryHandler';
+export { FunnelQueryHandler } from './funnelQueryHandler';
+export type { FunnelQueryResult, FunnelStageResult, FunnelStage } from './funnelQueryHandler';
+
